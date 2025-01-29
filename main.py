4bd1c5cb2ef4b4d005a406acd74a300c97304c83
shifas <sshifas333@gmail.com>
2025-01-29 11:37:20 +0530

    wrk done

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template,request
+import sqlite3
+con=sqlite3.connect("dbs.db")
+try:
+    con.execute("create table msg(name text,age int,email email)")
+except:
+    pass
+
+app=Flask(__name__)
+
+@app.route('/')
+def Home():
+    return render_template('index.html')
+
+app.run()
